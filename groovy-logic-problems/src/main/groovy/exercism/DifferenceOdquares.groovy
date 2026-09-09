ca878f2074df4733b10b116d47cdd6dc717bcf16
Gustavo Cardoso Costa <gustavocardoso0019@gmail.com>
2026-09-09 20:55:05 -0300

    solve problem 5 - DifferenceOfSquares

---
+++ groovy-logic-problems/src/main/groovy/exercism/DifferenceOdquares.groovy
@@ -0,0 +1,26 @@
+package exercism
+
+class DifferenceOfSquares {
+    BigInteger num
+
+    DifferenceOfSquares(num) {
+        this.num = num
+    }
+
+    BigInteger squareOfSum() {
+        return (((num + 1)*num)/2) ** 2
+    }
+
+    BigInteger sumOfSquares() {
+        BigInteger sum = 0;
+        for (int i=1; i<=num; i++) {
+            sum += (i ** 2)
+        }
+        return sum
+    }
+
+    BigInteger difference() {
+        return squareOfSum() - sumOfSquares()
+    }
+
+}
---
+++ groovy-logic-problems/src/test/groovy/exercism/DifferenceOfSquaresTest.groovy
@@ -0,0 +1,53 @@
+package exercism
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class DifferenceOfSquaresTest extends Specification {
+
+    void "Testing difference of square of ten first numbers"() {
+        given: "Number"
+        BigInteger num = 10
+        BigInteger response = 2640
+
+        when: "Testing"
+        DifferenceOfSquares clazz = new DifferenceOfSquares(num)
+        BigInteger result = clazz.difference()
+
+        then: "Comparison"
+        result == response
+    }
+
+    @Unroll("Can square the sum of the numbers up to #integer")
+    def "Can square the sum up to the given integer"() {
+        expect:
+        new DifferenceOfSquares(integer).squareOfSum() == expected
+        where:
+        integer || expected
+        1       || 1
+        5       || 225
+        100     || 25502500
+    }
+
+    @Unroll("Can sum the squares up to #integer")
+    def "Can sum the squares up to the given integer"() {
+        expect:
+        new DifferenceOfSquares(integer).sumOfSquares() == expected
+        where:
+        integer || expected
+        1       || 1
+        5       || 55
+        100     || 338350
+    }
+
+    @Unroll("Can subtract sum of squares from square of sum of #integer")
+    def "Can subtract sum of squares from square of sum"() {
+        expect:
+        new DifferenceOfSquares(integer).difference() == expected
+        where:
+        integer || expected
+        1       || 0
+        5       || 170
+        100     || 25164150
+    }
+}
